357c03f28343145803915edb492287b237cb5348
Jennifer Edmondson <jenny@jennyedmondson.com.au>
2012-02-23 09:03:08 +1100

    CONTRIB-3483 adobeconnect - Enforce meeting title length of 60 chars at time of activity creation, taking into account course group names if the activity is in group mode.

--- mod_form.php
+++ mod_form.php
@@ -38,11 +38,11 @@ class mod_adobeconnect_mod_form extends moodleform_mod {
     /// Adding the "general" fieldset, where all the common settings are showed
         $mform->addElement('header', 'general', get_string('general', 'form'));
 
-    /// Adding the standard "name" field
+    /// Adding the standard "name" field - limit of 60 chars on Adobe Connect server
         $mform->addElement('text', 'name', get_string('adobeconnectname', 'adobeconnect'), array('size'=>'64'));
         $mform->setType('name', PARAM_TEXT);
         $mform->addRule('name', null, 'required', null, 'client');
-        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 60), 'maxlength', 60, 'client');
 
     /// Adding the required "intro" field to hold the description of the instance
         $mform->addElement('htmleditor', 'intro', get_string('adobeconnectintro', 'adobeconnect'));
@@ -198,12 +198,37 @@ class mod_adobeconnect_mod_form extends moodleform_mod {
                 return $errors;
             }
 
-            // Check that the course has groups if group mode is on
-            if (0 != $data['groupmode']) { // Allow for multiple groups
+            // Need to be aware that groupings might not be turned on
+            if (!isset($data['groupingid']) || !$data['groupingid']) {
+                $groupingid = 0;
+            } else {
+                $groupingid = $data['groupingid'];
+            }
+
+            // Check that the course (or selected grouping) has groups if group mode is on
+            if (0 != $data['groupmode']) { // Only for group mode
                 // get all groups for the course
-                $crsgroups = groups_get_all_groups($COURSE->id);
+                $crsgroups = groups_get_all_groups($COURSE->id, 0, $groupingid);
                 if (empty($crsgroups)) {
                     $errors['groupmode'] = get_string('invalidgroupmode', 'adobeconnect');
+                    return $errors;
+                }
+            }
+
+            // Check that meeting title is not too long in group mode (need to take into account group name length)
+            if (0 != $data['groupmode']) { // Only for group mode
+                // get all groups for the course
+                $crsgroupsmaxnamelength = groups_get_all_groups($COURSE->id, 0, $groupingid);
+                array_walk($crsgroupsmaxnamelength, create_function('&$val', '$val = strlen($val->name);'));
+                //maximum course name is (meetingname - (1 + maxgroupnamelength))
+                $maxnamelength = 60 - 1 - max($crsgroupsmaxnamelength);
+
+                if (strlen($data['name']) > $maxnamelength) {
+                    if ($maxnamelength <1) {
+                        $errors['name'] = get_string('shortengroupnames', 'adobeconnect', array('max'=>max($crsgroupsmaxnamelength), 'maxallowed'=>60));
+                    } else {
+                        $errors['name'] = get_string('invalidnamelength', 'adobeconnect', $maxnamelength);
+                    }
                 }
             }
 
