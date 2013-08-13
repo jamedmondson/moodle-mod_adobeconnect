d15c4437e775042bd93c855e5424ff10f23b751e
Jennifer Edmondson <jenny@jennyedmondson.com.au>
2013-08-13 20:05:24 +1000

    CONTRIB-4103 adobeconnect - update to v9 check 'recordings' attribute not only defined, but non-empty string

--- locallib.php
+++ locallib.php
@@ -625,7 +625,9 @@ function aconnect_get_recordings($aconnect, $folderscoid, $sourcescoid) {
                                 $recordingvac9 = $innernodelist->item($x)->attributes->getNamedItem('duration');
                                 //In AC-8 and before, the recording length info is stored as its own element
                                 $recordingvac8 = $meetingdetail->getElementsByTagName('duration')->item(0);
-                                if (!is_null($recordingvac9) || !is_null($recordingvac8)) {
+                                //In AC9, not only recordings have a 'recording' attribute defined, but the majority are empty.
+                                // So check the attribute has a value (possibly 0, so can't use !empty()) 
+                                if ((!is_null($recordingvac9) && $recordingvac9->value !== '') || !is_null($recordingvac8)) {
 
                                     $j = (int) $domnode->nodeValue;
                                     $value = (!is_null($meetingdetail->getElementsByTagName('name'))) ?
